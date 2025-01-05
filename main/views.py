ca44ce6647c01cb416327e477f2c86d3faa92de2
LL1urace <rozhin_sasha@inbox.ru>
2025-01-05 19:02:32 +0700

    After adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
+from unicodedata import category
 from django.shortcuts import render
+
+from goods.models import Categories
+
 
 
 # Create your views here.
 def index(request):
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home',
         'content': 'Магазин мебели HOME',
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
