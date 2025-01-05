47626849cf6aa497268aeae8301f2e8f36d57fc4
LL1urace <rozhin_sasha@inbox.ru>
2025-01-06 00:59:51 +0700

    The first pagination option

--- goods/views.py
+++ goods/views.py
@@ -11,12 +11,13 @@ def catalog(request, category_slug):
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
+        # Код в уроке не работал
         # goods= get_object_or_404(Products.objects.filter(category__slug=category_slug))
         # Фильтруем товары в существующей категории
         goods = Products.objects.filter(category__slug=category_slug)
         if not goods.exists():
             raise Http404("No products found in this category.")
-        
+
     context = {
         'title': 'Home - Каталог',
         'goods': goods,
