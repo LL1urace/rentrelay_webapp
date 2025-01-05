166b7463b9437bef39eca24c9b45235ade06606c
LL1urace <rozhin_sasha@inbox.ru>
2025-01-06 01:00:10 +0700

    The first pagination option

--- goods/views.py
+++ goods/views.py
@@ -1,26 +1,25 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 
 
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
-        # Код в уроке не работал
-        # goods= get_object_or_404(Products.objects.filter(category__slug=category_slug))
-        # Фильтруем товары в существующей категории
-        goods = Products.objects.filter(category__slug=category_slug)
-        if not goods.exists():
-            raise Http404("No products found in this category.")
+        goods= get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         'title': 'Home - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': category_slug,
     }
     return render(request, 'goods/catalog.html', context)  # Убедитесь, что путь корректен
 
